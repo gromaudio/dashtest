92f6e079e15d0f76cfcc1887ca55293c0d20a169
Ihor K <qihor.k@gmail.com>
2016-09-17 19:17:28 +0300

    Update script

--- start_test.sh
+++ start_test.sh
@@ -3,6 +3,9 @@
 `export ADB_DEVICE_ARG="9fd6b98e"
 export ANDROID_HOME=/home/tolik/ADP/adt-bundle-linux-x86_64-20140702/sdk
 calabash-android run /home/tolik/calabash-test-android/15115.apk --format html --out S4.html ADB_DEVICE_ARG="9fd6b98e"`&
+`export ADB_DEVICE_ARG="TA9921ASBX"
+export ANDROID_HOME=/home/tolik/ADP/adt-bundle-linux-x86_64-20140702/sdk
+calabash-android run /home/tolik/calabash-test-android/15115.apk --format html --out Nexus5.html ADB_DEVICE_ARG="TA9921ASBX"`&
 `export ADB_DEVICE_ARG="060cb3a2005187d2"
 export ANDROID_HOME=/home/tolik/ADP/adt-bundle-linux-x86_64-20140702/sdk
 calabash-android run /home/tolik/calabash-test-android/15115.apk --format html --out Nexus5.html ADB_DEVICE_ARG="060cb3a2005187d2"`
